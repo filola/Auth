73b5da9863da095a6e9fe8b8cc686635aa51e66b
filola <hsoorang@naver.com>
2022-08-26 17:00:43 +0900

    [Apply] Entity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
+  imports: [TypeOrmModule.forFeature([])],
 })
 export class ProductsModule {}
--- src/users/user.repository.ts
+++ src/users/user.repository.ts
@@ -1,5 +1,8 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
+import { SignUpDto } from './dto/signup-user.dto';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signup(signUpDto: SignUpDto): Promise<void> {}
+}
