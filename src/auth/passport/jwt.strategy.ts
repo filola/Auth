8dd2820481c5960f5f23a4d8546f1bb75844d899
filola <hsoorang@naver.com>
2023-02-22 18:14:19 +0900

    [update]jwt strategy

--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -1,18 +1,25 @@
+import { Request } from 'express';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor() {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: process.env.JWT_SECRET,
-        });
-    }
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.ACCESS_TOKEN;
+        },
+      ]),
+      secretOrKey: process.env.JWT_SECRET,
+      passReqToCallback: true,
+    });
+  }
 
-    async validate(payload: any) {
-        console.log(payload);
-        return payload;
-    }
+  async validate(request: Request, payload: any) {
+    const accessToken = request.cookies?.ACCESS_TOKEN;
+    console.log('asetestsetse', payload);
+    return payload;
+  }
 }
